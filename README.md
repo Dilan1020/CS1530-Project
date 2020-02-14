1f24178acea708f422510b6010e62221be2d70f8
Biying Zhang <biz11@pitt.edu>
2020-02-13 20:23:00 -0500

    Empty Vue

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# CS1530-Project
+# CS1530 Project
+# yawa
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<!--script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script-->
+
+<script>
+
+</script>
+
+<style lang="scss">
+@import "../scss/global.scss";
+</style>
