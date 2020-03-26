dde7bd0bf3c7694690378da0a5e17deeab402297
Biying Zhang <biz11@pitt.edu>
2020-03-26 13:17:35 -0400

    Added newpost route and plus button

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { faAngleUp, faAngleDown, faPlusSquare } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import router from './router'
 import store from './store'
 
-library.add(faAngleUp, faAngleDown)
+library.add(faAngleUp, faAngleDown, faPlusSquare)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
---
+++ src/views/NewPost.vue
@@ -0,0 +1,13 @@
+<template>
+	<div style="top: 100px; position: absolute">
+		ahhhh
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+@import "../scss/global.scss";
+</style>
