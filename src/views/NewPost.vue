77008f553c220154d00160620a69879cfe51b4e6
sjm161 <35508204+sjm161@users.noreply.github.com>
2020-03-30 22:37:12 -0400

    Added NewPost content

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,6 +1,7 @@
 <template>
-	<div style="top: 100px; position: absolute">
-		ahhhh
+	<div style="top: 100px; position: absolute; width: 100vw; bottom: 0px">
+		<textarea class="newposttext"></textarea>
+		<button class="newpostbutton">Here is a button</button>
 	</div>
 </template>
 
